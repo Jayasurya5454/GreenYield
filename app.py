2de50362aaa403a15cc379dc362c36eccd9e5c30
jayasurya <jayasuryakarthi55@gmail.com>
2024-03-04 22:15:28 +0530

    commit

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
 from flask import Flask, render_template, request
 import pickle
+import gemini_api as gem
 import os
-
 app = Flask(__name__)
 
-# Define the path to the models directory
 MODELS_DIR = os.path.join(os.path.dirname(__file__), 'models')
 
-# Load the trained Naive Bayes model from the pickle file in the models directory
-MODEL_FILE_PATH = os.path.join(MODELS_DIR, 'naive_bayes_model.pkl')
+MODEL_FILE_PATH = 'naive_bayes_model.pkl'
 with open(MODEL_FILE_PATH, 'rb') as model_file:
     nb_classifier = pickle.load(model_file)
 
-# Define the label mapping
 LABEL_MAPPING = {'Apple': 0, 'Banana': 1, 'Blackgram': 2, 'Chickpea': 3, 'Coconut': 4, 'Coffee': 5, 'Cotton': 6,
                  'Grapes': 7, 'Jute': 8, 'Kidneybeans': 9, 'Lentil': 10, 'Maize': 11, 'Mango': 12, 'Mothbeans': 13,
                  'Mungbean': 14, 'Muskmelon': 15, 'Orange': 16, 'Papaya': 17, 'Pigeonpeas': 18, 'Pomegranate': 19,
                  'Rice': 20, 'Watermelon': 21}
-
-# Define a function to preprocess user input and make predictions
+predict_crop=""
 def predict_crop(attributes):
     attributes = [float(attr) for attr in attributes]
     prediction = nb_classifier.predict([attributes])
     predicted_crop_index = prediction[0]
     
-    # Convert numerical prediction to crop name using the label mapping
     predicted_crop_name = next(plant for plant, index in LABEL_MAPPING.items() if index == predicted_crop_index)
     
     return predicted_crop_name
@@ -35,12 +30,18 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get user input from the form
     user_input = request.form.to_dict()
     attributes = [user_input[key] for key in ['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall']]
-    
-    # Make predictions using the pre-trained model
+    global predicted_crop
     predicted_crop = predict_crop(attributes)
+
 
     return render_template('result.html', attributes=attributes, predicted_crop=predicted_crop)
 
+@app.route('/trigger_function1')
+def trigger_function1():
+    global predicted_crop
+    details=gem.mainn(predicted_crop)
+    return details
+if __name__ =="__main__":
+    app.run()
---
+++ gemini_api.py
@@ -0,0 +1,22 @@
+import os
+import google.generativeai as genai
+
+os.environ['GOOGLE_API_KEY'] = 'AIzaSyAE4pp7LbGcighoUahPhwA5QAHQKTTLlTE'
+
+
+genai.configure(api_key=os.environ.get('GOOGLE_API_KEY'))
+
+def format_text(text):
+    formatted_text = text.replace('.', '  ./n')
+    return formatted_text
+
+def mainn(name):
+    model = genai.GenerativeModel('gemini-pro')
+    k = f"I wants to plant {name} in my land so give me the details  like which season this crop should we grow , how much expence it take to cultivate for one actare , how much water needed to cultivate this crop , Expalin the process of cultivating this crop ."
+    response = model.generate_content(k)
+
+    # Assuming response.text is the content you want to print
+    formatted_content = format_text(response.text)
+    return formatted_content
+
+
